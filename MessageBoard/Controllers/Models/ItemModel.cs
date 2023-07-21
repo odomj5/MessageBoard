a9503dab60f6d251559246fa331f7c7ad0ce72fc
Jonathan <odomj5@gmail.com>
2023-07-21 16:35:06 -0700

    Setup repo

---
+++ MessageBoard/Controllers/Models/ItemModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MessageBoard.Controllers.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int ImageId { get; set; }
+
+        public int Ranking { get; set; }
+
+        public int ItemType { get; set; }
+
+
+    }
+}
